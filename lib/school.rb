c10957c590adbe87e7150cc1d725908f813b55af
sg3957 <sg3957@nyu.edu>
2018-10-10 12:59:08 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+
+attr_accessor :roster
+
+attr_reader :name
+
+  def initialize(name, roster={})
+    @name = name
+    @roster = roster
+
+  end
+
+  def add_student(name, grade)
+    roster[grade] = [] unless @roster[grade]
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, student_array|
+      @roster[grade] = student_array.sort
+    end
+  end
+
+
+
+
+end
